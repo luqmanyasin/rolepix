665a82b6f269d0a53263a680f399797ad778b3e4
Luqman Yasin <luqman.yasin@gmail.com>
2016-08-11 22:04:26 +0100

    larger images for the feed

--- js/javascript.js
+++ js/javascript.js
@@ -685,7 +685,8 @@ Insta Feed Start
     var userFeed = new Instafeed({
         get: 'user',
         userId: '3648847472',
-        accessToken: '3648847472.1677ed0.ed191534e104408792838dc0ada1b7ed'
+        accessToken: '3648847472.1677ed0.ed191534e104408792838dc0ada1b7ed',
+        resolution: 'standard_resolution'
     });
     userFeed.run();
 /* ==============================================
